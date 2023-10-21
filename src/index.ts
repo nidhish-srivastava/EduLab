ad7d99d56a456133d1e9d15c1b136b4314ae15d5
Nidhish Srivastava <nidhish0430@gmail.com>
2023-10-22 01:37:28 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,15 @@ dotenv.config();
 import { connectmongodb } from "./mongodb/connect";
 import { v2 as cloudinary } from "cloudinary";
 const port = process.env.PORT || 4000
-// const corsOptions = {
-//   // origin: "https://edu-lab-ruddy.vercel.app",
-//   // origin: "http://localhost:5173",
-//   // credentials: true,
-//   //access-control-allow-credentials:true
-//   // optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
-app.use(cors())
+const corsOptions = {
+  // origin: "https://edu-lab-ruddy.vercel.app",
+  origin: "http://localhost:5173",
+  credentials: true,
+  //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+// app.use(cors())
 app.use(express.json());
 
 cloudinary.config({
